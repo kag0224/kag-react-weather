b11c430b63792645e89baec3948f5992a119e291
kag0224 <101907323+kag0224@users.noreply.github.com>
2023-09-15 15:36:30 -0700

    Added github url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Kristy-Ann Gaor and is open-sourced on{" "}
+        <a
+          href="https://github.com/kag0224/kag-react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
