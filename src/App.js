633162d63012b7afa9eeddf7be8cd10efb0b13f5
kag0224 <101907323+kag0224@users.noreply.github.com>
2023-09-16 15:15:56 -0700

    start building weather app with content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Kristy-Ann Gaor and is open-sourced on{" "}
           <a
